c79db46d8a2aebf277fad5d30cba883ada74c0be
whoniiii <jhlee8024@gmail.com>
2016-06-03 09:34:02 +0900

    Commit to master
    Commit to master

---
+++ SPFDecompressor/fm_SPFDecompressor.Designer.vb
@@ -0,0 +1,166 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class SPFDecompressor
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(SPFDecompressor))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.gBtnSource = New System.Windows.Forms.Button()
+        Me.gTxtSource = New System.Windows.Forms.TextBox()
+        Me.gBtnDest = New System.Windows.Forms.Button()
+        Me.gTxtDest = New System.Windows.Forms.TextBox()
+        Me.gBtnProcess = New System.Windows.Forms.Button()
+        Me.glblProgress = New System.Windows.Forms.Label()
+        Me.gpgbProgress = New System.Windows.Forms.ProgressBar()
+        Me.gBtnCancel = New System.Windows.Forms.Button()
+        Me.gBbtnCompress = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(289, 19)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(79, 12)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Destination..."
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(10, 19)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(57, 12)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "Source..."
+        '
+        'gBtnSource
+        '
+        Me.gBtnSource.Location = New System.Drawing.Point(184, 34)
+        Me.gBtnSource.Name = "gBtnSource"
+        Me.gBtnSource.Size = New System.Drawing.Size(61, 21)
+        Me.gBtnSource.TabIndex = 1
+        Me.gBtnSource.Text = "Open..."
+        Me.gBtnSource.UseVisualStyleBackColor = True
+        '
+        'gTxtSource
+        '
+        Me.gTxtSource.Location = New System.Drawing.Point(14, 34)
+        Me.gTxtSource.Name = "gTxtSource"
+        Me.gTxtSource.Size = New System.Drawing.Size(163, 21)
+        Me.gTxtSource.TabIndex = 2
+        '
+        'gBtnDest
+        '
+        Me.gBtnDest.Location = New System.Drawing.Point(458, 34)
+        Me.gBtnDest.Name = "gBtnDest"
+        Me.gBtnDest.Size = New System.Drawing.Size(61, 21)
+        Me.gBtnDest.TabIndex = 1
+        Me.gBtnDest.Text = "Open..."
+        Me.gBtnDest.UseVisualStyleBackColor = True
+        '
+        'gTxtDest
+        '
+        Me.gTxtDest.Location = New System.Drawing.Point(293, 34)
+        Me.gTxtDest.Name = "gTxtDest"
+        Me.gTxtDest.Size = New System.Drawing.Size(158, 21)
+        Me.gTxtDest.TabIndex = 2
+        '
+        'gBtnProcess
+        '
+        Me.gBtnProcess.Location = New System.Drawing.Point(284, 128)
+        Me.gBtnProcess.Name = "gBtnProcess"
+        Me.gBtnProcess.Size = New System.Drawing.Size(117, 21)
+        Me.gBtnProcess.TabIndex = 3
+        Me.gBtnProcess.Text = "DeCompress"
+        Me.gBtnProcess.UseVisualStyleBackColor = True
+        '
+        'glblProgress
+        '
+        Me.glblProgress.Location = New System.Drawing.Point(351, 104)
+        Me.glblProgress.Name = "glblProgress"
+        Me.glblProgress.Size = New System.Drawing.Size(170, 13)
+        Me.glblProgress.TabIndex = 4
+        Me.glblProgress.Text = "0"
+        Me.glblProgress.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'gpgbProgress
+        '
+        Me.gpgbProgress.Location = New System.Drawing.Point(14, 78)
+        Me.gpgbProgress.Name = "gpgbProgress"
+        Me.gpgbProgress.Size = New System.Drawing.Size(510, 21)
+        Me.gpgbProgress.TabIndex = 5
+        '
+        'gBtnCancel
+        '
+        Me.gBtnCancel.Location = New System.Drawing.Point(407, 128)
+        Me.gBtnCancel.Name = "gBtnCancel"
+        Me.gBtnCancel.Size = New System.Drawing.Size(117, 21)
+        Me.gBtnCancel.TabIndex = 6
+        Me.gBtnCancel.Text = "Exit"
+        Me.gBtnCancel.UseVisualStyleBackColor = True
+        '
+        'gBbtnCompress
+        '
+        Me.gBbtnCompress.Location = New System.Drawing.Point(161, 128)
+        Me.gBbtnCompress.Name = "gBbtnCompress"
+        Me.gBbtnCompress.Size = New System.Drawing.Size(117, 21)
+        Me.gBbtnCompress.TabIndex = 7
+        Me.gBbtnCompress.Text = "Compress"
+        Me.gBbtnCompress.UseVisualStyleBackColor = True
+        '
+        'SPFDecompressor
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(535, 192)
+        Me.Controls.Add(Me.gBbtnCompress)
+        Me.Controls.Add(Me.glblProgress)
+        Me.Controls.Add(Me.gBtnCancel)
+        Me.Controls.Add(Me.gpgbProgress)
+        Me.Controls.Add(Me.gBtnProcess)
+        Me.Controls.Add(Me.gTxtDest)
+        Me.Controls.Add(Me.gTxtSource)
+        Me.Controls.Add(Me.gBtnDest)
+        Me.Controls.Add(Me.gBtnSource)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "SPFDecompressor"
+        Me.Text = "SPFDecompressor"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents gBtnSource As System.Windows.Forms.Button
+    Friend WithEvents gTxtSource As System.Windows.Forms.TextBox
+    Friend WithEvents gBtnDest As System.Windows.Forms.Button
+    Friend WithEvents gTxtDest As System.Windows.Forms.TextBox
+    Friend WithEvents gBtnProcess As System.Windows.Forms.Button
+    Friend WithEvents glblProgress As System.Windows.Forms.Label
+    Friend WithEvents gpgbProgress As System.Windows.Forms.ProgressBar
+    Friend WithEvents gBtnCancel As System.Windows.Forms.Button
+    Friend WithEvents gBbtnCompress As Button
+End Class
---
+++ SPFDecompressor/fm_SPFDecompressor.vb
@@ -0,0 +1,173 @@
+﻿Imports System.IO
+Imports System.Threading
+Imports SPF.Utilities
+
+Public Class SPFDecompressor
+
+    Public Delegate Sub SetProgCallBack(ByVal lintPram As Integer)
+    Public Delegate Sub SetLabelCallBack(ByVal lstrPram As String)
+    Public Delegate Sub StartCallBack(ByVal lintMax As Integer)
+    Public Delegate Sub ExitCallBack()
+
+    Private t1 As System.Threading.Thread
+    Dim lstwLog As StreamWriter
+
+    Private Sub gBtnSource_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles gBtnSource.Click
+        Dim lfdlg As FolderBrowserDialog = New FolderBrowserDialog()
+        If lfdlg.ShowDialog() = DialogResult.OK Then
+            gTxtSource.Text = lfdlg.SelectedPath
+        End If
+    End Sub
+
+    Private Sub gBtnDest_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles gBtnDest.Click
+        Dim lfdlg As FolderBrowserDialog = New FolderBrowserDialog()
+        If lfdlg.ShowDialog() = DialogResult.OK Then
+            gTxtDest.Text = lfdlg.SelectedPath
+        End If
+    End Sub
+    Private Sub gBtnProcess_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles gBtnProcess.Click
+        Try
+            lstwLog = New StreamWriter(gTxtDest.Text + "\result.log")
+            t1 = New System.Threading.Thread(New ThreadStart(AddressOf process))
+            t1.Start()
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub SetProgBar(ByVal lintPram As Integer)
+        If Me.gpgbProgress.InvokeRequired Then
+            Dim dele As New SetProgCallBack(AddressOf SetProgBar)
+            Me.Invoke(dele, New Object() {lintPram})
+        Else
+            Me.gpgbProgress.Value = lintPram
+        End If
+    End Sub
+
+
+    Private Sub SetLabel(ByVal lstrPram As String)
+        If (Me.glblProgress.InvokeRequired) Then
+            Dim dele As New SetLabelCallBack(AddressOf SetLabel)
+            Me.Invoke(dele, New Object() {lstrPram})
+        Else
+            Me.glblProgress.Text = lstrPram
+        End If
+
+    End Sub
+
+
+    Private Sub initControl(ByVal lintMax As Integer)
+        Me.gBtnProcess.Text = "processing..."
+        Me.gBtnProcess.Enabled = False
+        Me.gBtnSource.Enabled = False
+        Me.gBtnDest.Enabled = False
+        Me.gpgbProgress.Maximum = lintMax + 1
+    End Sub
+
+    Private Sub endControl()
+        Me.gBtnProcess.Text = "Process"
+        Me.gBtnProcess.Enabled = True
+        Me.gBtnSource.Enabled = True
+        Me.gBtnDest.Enabled = True
+    End Sub
+
+    Private Sub [Start](ByVal lintMax As Integer)
+        Dim dele As New StartCallBack(AddressOf initControl)
+        Me.Invoke(dele, New Object() {lintMax})
+    End Sub
+
+    Private Sub [Exit]()
+        Dim dele As New ExitCallBack(AddressOf endControl)
+        Me.Invoke(dele)
+    End Sub
+
+
+    Private Sub process()
+        Try
+            Dim iintCnt As Integer = 0
+            Dim lstrSource As String = gTxtSource.Text
+            Dim lstrDest As String = gTxtDest.Text
+            Dim fileEntries As String() = System.IO.Directory.GetFiles(lstrSource, "*", System.IO.SearchOption.AllDirectories)
+            Me.[Start](fileEntries.Count.ToString())
+
+            For Each fileName As String In fileEntries
+                Try
+                    FileUtilities.DeCompressFile(fileName, lstrDest + fileName.Replace(lstrSource, ""))
+
+                    iintCnt = iintCnt + 1
+                    SetProgBar(iintCnt)
+                    SetLabel(iintCnt.ToString() + "/" + fileEntries.Count.ToString())
+                Catch ex As Exception
+                    lstwLog.WriteLine("[Error] FileName=" + fileName + ", Reason=" + ex.Message)
+                End Try
+            Next
+
+            Me.[Exit]()
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+
+    End Sub
+
+    Private Sub gBtnCancel_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles gBtnCancel.Click
+        Me.SPFDecompressor_FormClosing(Nothing, Nothing)
+        Me.Dispose()
+        Me.Close()
+    End Sub
+
+    Private Sub SPFDecompressor_FormClosing(ByVal sender As System.Object, ByVal e As System.Windows.Forms.FormClosingEventArgs) Handles MyBase.FormClosing
+
+        Try
+            Dim fi As FileInfo = New FileInfo(gTxtDest.Text + "\result.log")
+            If fi.Length = 0 Then
+                lstwLog.Close()
+                fi.Delete()
+            Else
+                MessageBox.Show("Error occurred during decompress. See the log for details.(" + gTxtDest.Text + "\result.log" + ")")
+            End If
+            lstwLog.Close()
+        Catch ex As Exception
+        End Try
+    End Sub
+
+    Private Sub gBbtnCompress_Click(sender As Object, e As EventArgs) Handles gBbtnCompress.Click
+        Try
+            lstwLog = New StreamWriter(gTxtDest.Text + "\result.log")
+            t1 = New System.Threading.Thread(New ThreadStart(AddressOf compress))
+            t1.Start()
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub compress()
+        Try
+            Dim iintCnt As Integer = 0
+            Dim lstrSource As String = gTxtSource.Text
+            Dim lstrDest As String = gTxtDest.Text
+            Dim fileEntries As String() = System.IO.Directory.GetFiles(lstrSource, "*", System.IO.SearchOption.AllDirectories)
+            Me.[Start](fileEntries.Count.ToString())
+
+            For Each fileName As String In fileEntries
+                Try
+
+                    FileUtilities.CompressFile(fileName, lstrDest + fileName.Replace(lstrSource, ""))
+
+                    iintCnt = iintCnt + 1
+                    SetProgBar(iintCnt)
+                    SetLabel(iintCnt.ToString() + "/" + fileEntries.Count.ToString())
+                Catch ex As Exception
+                    lstwLog.WriteLine("[Error] FileName=" + fileName + ", Reason=" + ex.Message)
+                End Try
+            Next
+
+            Me.[Exit]()
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+
+    End Sub
+
+End Class
